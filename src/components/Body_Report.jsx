f6f592ca8872602ce587517775ef9ffedc137baf
LeDuyhungC <hle4831@gmail.com>
2025-04-18 14:23:30 -0700

    Report template

--- src/components/Body_Report.jsx
+++ src/components/Body_Report.jsx
@@ -4,8 +4,18 @@ export default function Body_Report() {
     return (
         <>
         <body>
-            <div>
+            <div className="report_div">
                 <h1>Report</h1>
+                <form>
+                    
+                    <section>First Name: <input type = "text"></input></section>
+                    <section>Last Name: <input type = "text"></input></section>
+                    <section>Personnel Type: <input type = "text"></input></section>
+                    <section>Emergency: <input type = "text"></input></section>
+                    <section>Location: <input type = "text"></input></section>
+                    <section>Date: <input type = "date"></input>Time: <input type = "time"></input></section>
+                    <input type = "submit"></input>
+                </form>
             </div>
         </body>
         </>
